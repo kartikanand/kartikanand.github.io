8e7fcb50620917d881d941b9fbf6c92e4f11a4ed
Kartik Anand <kartikanand1992@gmail.com>
2015-08-03 00:01:41 +0530

    When displaying one section hide others

--- static/dev/main.js
+++ static/dev/main.js
@@ -2,12 +2,22 @@ window.onload = function () {
       var blog = document.getElementById('blog');
       blog.addEventListener('click', function (event) {
         event.preventDefault();
+        hideSections();
         document.getElementById('blog-section').style.display = "block";
       }, false);
 
       var contact = document.getElementById('contact');
       contact.addEventListener('click', function (event) {
         event.preventDefault();
+        hideSections();
         document.getElementById('contact-section').style.display = "block";
       }, false);
+}
+
+function hideSections () {
+    var sections = document.getElementsByTagName("section");
+
+    for (var i=0; i<sections.length; i++) {
+        sections[i].style.display = "none";
+    }
 }
