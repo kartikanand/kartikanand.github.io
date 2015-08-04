9f1bcc482d238733d37f9717333643615616904d
Kartik Anand <kartikanand1992@gmail.com>
2015-08-04 22:16:28 +0530

    Removed redundant JS

--- static/dev/main.js
+++ static/dev/main.js
@@ -1,3 +1,5 @@
+var onDisplay = document.getElementById('section-0');
+
 window.onload = function () {
 
     var buttons = document.getElementsByClassName('btn');
@@ -6,20 +8,11 @@ window.onload = function () {
     }
 }
 
-function hideSections () {
-    document.getElementById('section-0').style.display = "none";
-    
-    var sections = document.getElementsByClassName("hidden-section");
-
-    for (var i=0; i<sections.length; i++) {
-        sections[i].style.display = "none";
-    }
-}
-
 function addEventListenerToSection (buttons, i) {
     buttons[i].addEventListener('click', function (event) {
         event.preventDefault();
-        hideSections(); // To hide previously displayed section
+        onDisplay.style.display = 'none';
         document.getElementById('section-'+i).style.display = "block";
+        onDisplay = document.getElementById('section-'+i);
     }, false);
 }
