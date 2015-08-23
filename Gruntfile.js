7349232e11a6c717dc198f553a8f0867b499d649
Kartik Anand <kartikanand1992@gmail.com>
2015-08-23 21:53:22 +0530

    Clean up in directories; moved to local font-awesome file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,27 @@ module.exports = function(grunt) {
         sass: {
             dist: {
             	files: {
-            		'static/prod/styles.css': 'static/dev/styles.scss'
+            		'static/prod/css/styles.css': 'static/dev/css/styles.scss'
             	}
             }
         },
         cssmin: {
         	css:{
-        		src: 'static/prod/styles.css',
-        		dest: 'static/prod/styles.min.css'
-        	}
+        		src: 'static/prod/css/styles.css',
+        		dest: 'static/prod/css/styles.min.css'
+        	},
+            font_awesome: {
+                src: 'static/prod/css/font-awesome.css',
+                dest: 'static/prod/css/font-awesome.min.css'
+            }
+
         },
         watch: {
             options: {
                 livereload: true,
             },
             css: {
-            	files: ['static/dev/styles.scss', 'index.html'],
+            	files: ['static/dev/css/styles.scss', 'index.html'],
             	tasks: ['sass', 'cssmin']
             }
         }
@@ -29,5 +34,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', ['sass:dist', 'cssmin:css']);
+	grunt.registerTask('default', ['cssmin:font_awesome']);
 };
